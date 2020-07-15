46c56b44b5c2eec3ddaf338ed4bbf5721549221a
Katarina <katarina@udel.edu>
2020-07-14 21:21:36 -0400

    added first parts of MCV

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,13 +2,32 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  private ABRxn: any;
+  private ABRxnHighlight: any;
+  private background: any;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
+    this.background=this.add.image(200, 200, "bluebackground");
+    this.background.setScale(2.0);
+    
+    this.ABRxnHighlight=this.add
+    .image(100,100, "A+B")
+    .setScale(0.3)
+    .setTintFill(0x033dfc);
+
+    this.ABRxn=this.add
+    .image(100, 100, "A+B")
+    .setScale(.3);
+    //use mouse over and set tint fill to highlight when the image is moused over
+
+    
+
+    
+    //this.exampleObject = new ExampleObject(this, 0, 0);
   }
 
   update() {
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,6 +4,10 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("shirt", "assets/images/shirt.png");
+    this.load.image("bluebackground", "assets/images/blue.png");
+    this.load.image("A+B", "assets/images/A + B.png");
+    //this.load.image("imageName", "assets/images/imageName.png");
   }
 
   create() {

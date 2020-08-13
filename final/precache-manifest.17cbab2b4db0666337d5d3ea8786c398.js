8decd5b95edd7c61cdbd3f6b628606c89edf511b
Katarina <katarina@udel.edu>
2020-08-13 16:19:13 -0400

    Updates

---
+++ final/precache-manifest.17cbab2b4db0666337d5d3ea8786c398.js
@@ -0,0 +1,594 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0f27c5f5dfd7a773a59f7942e6f947",
+    "url": "/LimitingReagentSim/finalassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/LimitingReagentSim/finalassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/LimitingReagentSim/finalassets/font/font.xml"
+  },
+  {
+    "revision": "700f98875d4d397dd4354a39de3d4b8d",
+    "url": "/LimitingReagentSim/finalassets/font/font5.fnt"
+  },
+  {
+    "revision": "003b1b6b966b034a248846073064a4b9",
+    "url": "/LimitingReagentSim/finalassets/font/font5_0.png"
+  },
+  {
+    "revision": "bc1faa44b91ef4f48df918b8ebfa702b",
+    "url": "/LimitingReagentSim/finalassets/font/font6.fnt"
+  },
+  {
+    "revision": "2d091dacaff449cbf0e1f6ae93c12b71",
+    "url": "/LimitingReagentSim/finalassets/font/font6_0.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/LimitingReagentSim/finalassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/LimitingReagentSim/finalassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/LimitingReagentSim/finalassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/LimitingReagentSim/finalassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/LimitingReagentSim/finalassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "1672ff0a4fb37c0456144585ba5c766e",
+    "url": "/LimitingReagentSim/finalassets/images/1button.png"
+  },
+  {
+    "revision": "21a1b6ed2cc2117b67893d0b67859551",
+    "url": "/LimitingReagentSim/finalassets/images/2button.png"
+  },
+  {
+    "revision": "5b4704e67c5fed43b7c2eb97c1e9ae83",
+    "url": "/LimitingReagentSim/finalassets/images/3button.png"
+  },
+  {
+    "revision": "7c13a35d89bbf04695cd28772dd50b5f",
+    "url": "/LimitingReagentSim/finalassets/images/A + B.png"
+  },
+  {
+    "revision": "3d27d2ed0748a52b5844cf869d6c045b",
+    "url": "/LimitingReagentSim/finalassets/images/ABPdt.png"
+  },
+  {
+    "revision": "875015a5cadaa507bd883f119e3730b5",
+    "url": "/LimitingReagentSim/finalassets/images/ABbutton.png"
+  },
+  {
+    "revision": "e6a9cbc9a007b6df7f4a8e8c8a3d083c",
+    "url": "/LimitingReagentSim/finalassets/images/C + D.png"
+  },
+  {
+    "revision": "768c05e9ad3fcc59c0abff5b8f5f96d4",
+    "url": "/LimitingReagentSim/finalassets/images/C.png"
+  },
+  {
+    "revision": "49cff6391494bc17f425b945144aa93e",
+    "url": "/LimitingReagentSim/finalassets/images/C2H4Rxn.png"
+  },
+  {
+    "revision": "874510b37433a8cc07b351607bb5a7e9",
+    "url": "/LimitingReagentSim/finalassets/images/CDPdt.png"
+  },
+  {
+    "revision": "8ba6200f844a64b0b97a2f8563efd56b",
+    "url": "/LimitingReagentSim/finalassets/images/CDbutton.png"
+  },
+  {
+    "revision": "2de58f54a0117c34d48d9c0e9feb1c28",
+    "url": "/LimitingReagentSim/finalassets/images/CO2.png"
+  },
+  {
+    "revision": "26999e0f13d1d02392e983954f63a444",
+    "url": "/LimitingReagentSim/finalassets/images/CO2molec.png"
+  },
+  {
+    "revision": "2ee7972b6192c98125788215cfa8c408",
+    "url": "/LimitingReagentSim/finalassets/images/CO2stack.png"
+  },
+  {
+    "revision": "c229d24b2419387925dd361c3b74c838",
+    "url": "/LimitingReagentSim/finalassets/images/CORxn.png"
+  },
+  {
+    "revision": "ade1ae30d33512de2305f5bb16cfbffc",
+    "url": "/LimitingReagentSim/finalassets/images/Cstack.png"
+  },
+  {
+    "revision": "62500132a695203588ae6e20c56824d1",
+    "url": "/LimitingReagentSim/finalassets/images/E + F.png"
+  },
+  {
+    "revision": "2fd515c16089fb69f70ea1aa72229659",
+    "url": "/LimitingReagentSim/finalassets/images/EFPdt.png"
+  },
+  {
+    "revision": "fdd6113b6754cabe7e2d5b4e7d45e155",
+    "url": "/LimitingReagentSim/finalassets/images/EFbutton.png"
+  },
+  {
+    "revision": "2664e10ea57040a3b361d603fc864cd3",
+    "url": "/LimitingReagentSim/finalassets/images/Fe.png"
+  },
+  {
+    "revision": "37a76e25cd3ae8e30499d39f1cdc7838",
+    "url": "/LimitingReagentSim/finalassets/images/Fe2O3.png"
+  },
+  {
+    "revision": "e8920194527d5b14f1f0183595568b8f",
+    "url": "/LimitingReagentSim/finalassets/images/Fe2O3Cbutton.png"
+  },
+  {
+    "revision": "b14b6113b64f9a314aa849a4a79a7d51",
+    "url": "/LimitingReagentSim/finalassets/images/Fe2O3stack.png"
+  },
+  {
+    "revision": "2b9bf744dfe31fc073a620ceee674b54",
+    "url": "/LimitingReagentSim/finalassets/images/FeCl2Rxn.png"
+  },
+  {
+    "revision": "16eb8ecfe781831961aae84e65c5c8fa",
+    "url": "/LimitingReagentSim/finalassets/images/FeRxn.png"
+  },
+  {
+    "revision": "7a5efa7ae35425a55325a22786647bf0",
+    "url": "/LimitingReagentSim/finalassets/images/Festack.png"
+  },
+  {
+    "revision": "412bbc91f35ba66021862a7dccc40bd3",
+    "url": "/LimitingReagentSim/finalassets/images/H2.png"
+  },
+  {
+    "revision": "a20ccaf2aa5bb0cd4311b2729277af3c",
+    "url": "/LimitingReagentSim/finalassets/images/H2O.png"
+  },
+  {
+    "revision": "b2e595a4aea87163216b77727dddc930",
+    "url": "/LimitingReagentSim/finalassets/images/H2Omolec.png"
+  },
+  {
+    "revision": "60516e008eac1cdc900a67a2f44ccdf8",
+    "url": "/LimitingReagentSim/finalassets/images/H2Ostack.png"
+  },
+  {
+    "revision": "698e288323e0a642906cc7a9689427aa",
+    "url": "/LimitingReagentSim/finalassets/images/H2molec.png"
+  },
+  {
+    "revision": "8989de16e7fd748c6fce76f460215b99",
+    "url": "/LimitingReagentSim/finalassets/images/H2stack.png"
+  },
+  {
+    "revision": "68e8ffc234c2e379d69f639660021abb",
+    "url": "/LimitingReagentSim/finalassets/images/IDLR.png"
+  },
+  {
+    "revision": "f43404d1d189dbff52a843d5aee79d2e",
+    "url": "/LimitingReagentSim/finalassets/images/LRLabel.png"
+  },
+  {
+    "revision": "bdc2742d3fb7e7f98d4ab955bc387558",
+    "url": "/LimitingReagentSim/finalassets/images/LRhelp.png"
+  },
+  {
+    "revision": "af105ee1034fa1cbb23b0a47c3151d3b",
+    "url": "/LimitingReagentSim/finalassets/images/LRhelp2.png"
+  },
+  {
+    "revision": "1115110e554fa368c51d1b4f0800d995",
+    "url": "/LimitingReagentSim/finalassets/images/MCV Label.png"
+  },
+  {
+    "revision": "dadb0e5bb73fc2efe03b61a1744ea746",
+    "url": "/LimitingReagentSim/finalassets/images/MWbox.png"
+  },
+  {
+    "revision": "ecfad4edcd93f0ada798688ec66fcc41",
+    "url": "/LimitingReagentSim/finalassets/images/NH3Rxn.png"
+  },
+  {
+    "revision": "3fad88df954277e2067354e9e081367a",
+    "url": "/LimitingReagentSim/finalassets/images/NaClRxn.png"
+  },
+  {
+    "revision": "8022763fd0e9ee631603c246ae8a3431",
+    "url": "/LimitingReagentSim/finalassets/images/NaRxn.png"
+  },
+  {
+    "revision": "947cc1db6139a394b9ecd91094c79fda",
+    "url": "/LimitingReagentSim/finalassets/images/O2.png"
+  },
+  {
+    "revision": "ccf480b6bebdcf57ee39bca0f424e66d",
+    "url": "/LimitingReagentSim/finalassets/images/O2H2button.png"
+  },
+  {
+    "revision": "b09c429544f052d56c6a066eb20ec4dc",
+    "url": "/LimitingReagentSim/finalassets/images/O2Rxn.png"
+  },
+  {
+    "revision": "c32264cdbb1fb0e8cad5cdb50adca776",
+    "url": "/LimitingReagentSim/finalassets/images/O2molec.png"
+  },
+  {
+    "revision": "5b0e7f92ff7117628a49287f7f3d2232",
+    "url": "/LimitingReagentSim/finalassets/images/O2stack.png"
+  },
+  {
+    "revision": "dd8d7c599a7f4fd4509c88c55c21ac70",
+    "url": "/LimitingReagentSim/finalassets/images/PY.png"
+  },
+  {
+    "revision": "cfeefe48143755c219e0183ec97a4ab8",
+    "url": "/LimitingReagentSim/finalassets/images/PYExampleFull.png"
+  },
+  {
+    "revision": "e63cb887467565d153338b79d3be9aa0",
+    "url": "/LimitingReagentSim/finalassets/images/PYexample.png"
+  },
+  {
+    "revision": "516aac655ffbd0ec6275c21bca811dd9",
+    "url": "/LimitingReagentSim/finalassets/images/PYexample2.png"
+  },
+  {
+    "revision": "11a4a00ceb9a318ff77b8e16896704ff",
+    "url": "/LimitingReagentSim/finalassets/images/PYexample3.png"
+  },
+  {
+    "revision": "fc04025994820ddd8bd6603cff9a7f1c",
+    "url": "/LimitingReagentSim/finalassets/images/Pdt.png"
+  },
+  {
+    "revision": "5170f02e4f8029c99dfa4e35d9873456",
+    "url": "/LimitingReagentSim/finalassets/images/PdtFormed.png"
+  },
+  {
+    "revision": "e4c075309ea8913806a2524a94547b42",
+    "url": "/LimitingReagentSim/finalassets/images/RLHelp.png"
+  },
+  {
+    "revision": "ae2e37c0b81d5d11349d94354bf48fbd",
+    "url": "/LimitingReagentSim/finalassets/images/ReactantLeft.png"
+  },
+  {
+    "revision": "db63c06bb4ae2a3d817c53f78a0a0003",
+    "url": "/LimitingReagentSim/finalassets/images/SO2Rxn.png"
+  },
+  {
+    "revision": "4d8c0b61daa1131620f102b36ba30b1d",
+    "url": "/LimitingReagentSim/finalassets/images/SiRxn.png"
+  },
+  {
+    "revision": "fcfea17188d109f938887b993f87e297",
+    "url": "/LimitingReagentSim/finalassets/images/absGraph.png"
+  },
+  {
+    "revision": "128c8f8609ab1c38e9a65618a2d6b549",
+    "url": "/LimitingReagentSim/finalassets/images/absGraphAB.png"
+  },
+  {
+    "revision": "cb3b2292b3ebd09031dc91effb752a7f",
+    "url": "/LimitingReagentSim/finalassets/images/absGraphCD.png"
+  },
+  {
+    "revision": "fddd50e4c73157e3e99eb7d9ff9e0b32",
+    "url": "/LimitingReagentSim/finalassets/images/absGraphEF.png"
+  },
+  {
+    "revision": "27cfc6f81130f54ea147ae77f8394cb3",
+    "url": "/LimitingReagentSim/finalassets/images/absGraphGH.png"
+  },
+  {
+    "revision": "a1b1b08e4d1cefe07877d902f19f1456",
+    "url": "/LimitingReagentSim/finalassets/images/backButton.png"
+  },
+  {
+    "revision": "c5f66fa44c8108ecec0f6341fd3567ce",
+    "url": "/LimitingReagentSim/finalassets/images/blackBox.png"
+  },
+  {
+    "revision": "f60fa486512ee47cb2c3bc0aa9427d44",
+    "url": "/LimitingReagentSim/finalassets/images/blackCircle.png"
+  },
+  {
+    "revision": "ec2d322cc3894d03054fc006d109c0b6",
+    "url": "/LimitingReagentSim/finalassets/images/blue.png"
+  },
+  {
+    "revision": "a2918e7d90a5ff38b3f6325769ae1bd2",
+    "url": "/LimitingReagentSim/finalassets/images/blueArrow.png"
+  },
+  {
+    "revision": "e29205d76db97c78356c9f22b40db20d",
+    "url": "/LimitingReagentSim/finalassets/images/box.png"
+  },
+  {
+    "revision": "0d1e6bc9ea4529a940097fe55678e294",
+    "url": "/LimitingReagentSim/finalassets/images/compoundBox.png"
+  },
+  {
+    "revision": "76c052bcc6c830cb17a145598fc93e59",
+    "url": "/LimitingReagentSim/finalassets/images/correct.png"
+  },
+  {
+    "revision": "e758ffbb4eb406a0f5be8318380838d0",
+    "url": "/LimitingReagentSim/finalassets/images/correct2.png"
+  },
+  {
+    "revision": "05714bcbd040aff08b4e5476993b15df",
+    "url": "/LimitingReagentSim/finalassets/images/cuvetteOutline.png"
+  },
+  {
+    "revision": "7751136a31c2c7de9aa5842412b3d4a0",
+    "url": "/LimitingReagentSim/finalassets/images/cuvetteOutline2.png"
+  },
+  {
+    "revision": "a1af7e96379907c6efe4d52d7cf6ee6d",
+    "url": "/LimitingReagentSim/finalassets/images/decimal.png"
+  },
+  {
+    "revision": "c758adb064dfd0f8514a237e6dde87df",
+    "url": "/LimitingReagentSim/finalassets/images/downArrow.png"
+  },
+  {
+    "revision": "a645cdb68aa9e5d3b03055206e85043a",
+    "url": "/LimitingReagentSim/finalassets/images/emptyBeaker.png"
+  },
+  {
+    "revision": "efe5cd935676b162cf2c980bd3b41ae2",
+    "url": "/LimitingReagentSim/finalassets/images/emptyCuvette.png"
+  },
+  {
+    "revision": "83e767c881ef349af135631740ae5418",
+    "url": "/LimitingReagentSim/finalassets/images/emptyCuvette2.png"
+  },
+  {
+    "revision": "94f15ae523d6b29746ad68392bf1140b",
+    "url": "/LimitingReagentSim/finalassets/images/emptyVial.png"
+  },
+  {
+    "revision": "e5deba009cf3bb643c75c4cc06d21f31",
+    "url": "/LimitingReagentSim/finalassets/images/emptyVial2.png"
+  },
+  {
+    "revision": "38311ec096bfe673a583d3c18b9c8310",
+    "url": "/LimitingReagentSim/finalassets/images/exampleButton.png"
+  },
+  {
+    "revision": "b5a903b0845355939bfccb68b4d3b0ba",
+    "url": "/LimitingReagentSim/finalassets/images/fullBeaker.png"
+  },
+  {
+    "revision": "6d62936464c02d83f867000c171a411e",
+    "url": "/LimitingReagentSim/finalassets/images/fullCuvette.png"
+  },
+  {
+    "revision": "55b24f6609dd6b065818ecf954a4310c",
+    "url": "/LimitingReagentSim/finalassets/images/fullCuvette2.png"
+  },
+  {
+    "revision": "311e7242c2422683a23c76e0d90fe17a",
+    "url": "/LimitingReagentSim/finalassets/images/fullVial.png"
+  },
+  {
+    "revision": "e205fbc0f97eaf4a1db48bcfbbf9b712",
+    "url": "/LimitingReagentSim/finalassets/images/gramButton.png"
+  },
+  {
+    "revision": "10e2d4b8d0413e630cbcec96285f0174",
+    "url": "/LimitingReagentSim/finalassets/images/graphButton.png"
+  },
+  {
+    "revision": "e6d7f1200beec1d712c30246816d3214",
+    "url": "/LimitingReagentSim/finalassets/images/helpButton.png"
+  },
+  {
+    "revision": "b2d9994322796ab3da10b7dff464cd10",
+    "url": "/LimitingReagentSim/finalassets/images/leftovers.png"
+  },
+  {
+    "revision": "37f683ca1b30bc61a72629fb58918601",
+    "url": "/LimitingReagentSim/finalassets/images/mLsA.png"
+  },
+  {
+    "revision": "80802513d0a8f0818998194f271ee31b",
+    "url": "/LimitingReagentSim/finalassets/images/mLsB.png"
+  },
+  {
+    "revision": "afca1c80d8f56e0e16210c5beacbfbe1",
+    "url": "/LimitingReagentSim/finalassets/images/mLsC.png"
+  },
+  {
+    "revision": "960987c8299bc07af581f37577295d49",
+    "url": "/LimitingReagentSim/finalassets/images/mLsD.png"
+  },
+  {
+    "revision": "3fc9bdd4dd8d62526963804e65c39395",
+    "url": "/LimitingReagentSim/finalassets/images/mLsE.png"
+  },
+  {
+    "revision": "7749853c8c9865df307fd6cc2c03d9e1",
+    "url": "/LimitingReagentSim/finalassets/images/mLsF.png"
+  },
+  {
+    "revision": "86d305f9dcd3e0c0baa794875f607c41",
+    "url": "/LimitingReagentSim/finalassets/images/mLsG.png"
+  },
+  {
+    "revision": "e3f2030f060fcd89265206933bbed1c4",
+    "url": "/LimitingReagentSim/finalassets/images/mLsH.png"
+  },
+  {
+    "revision": "005d6e7cdf8611aade0e872ae74db1d9",
+    "url": "/LimitingReagentSim/finalassets/images/mainButton.png"
+  },
+  {
+    "revision": "2bb5937263985678706af0e81dd800cf",
+    "url": "/LimitingReagentSim/finalassets/images/mixSolButton.png"
+  },
+  {
+    "revision": "c51fe17a588dc6527e6dd8096dd33f9e",
+    "url": "/LimitingReagentSim/finalassets/images/moleculeButton.png"
+  },
+  {
+    "revision": "79e3da3c9f62a5f296a505261aa135fb",
+    "url": "/LimitingReagentSim/finalassets/images/nextButton.png"
+  },
+  {
+    "revision": "0e85bdefa51d36e07384650c666f514f",
+    "url": "/LimitingReagentSim/finalassets/images/noMoles.png"
+  },
+  {
+    "revision": "7e829d07bdc66f0e3f3c1e149a1550af",
+    "url": "/LimitingReagentSim/finalassets/images/pBox.png"
+  },
+  {
+    "revision": "834359f4f9503312fd16b33db2a725e2",
+    "url": "/LimitingReagentSim/finalassets/images/practice.png"
+  },
+  {
+    "revision": "027e130b98ef7b5860f72a08baf27b7a",
+    "url": "/LimitingReagentSim/finalassets/images/precipButton.png"
+  },
+  {
+    "revision": "24e5a42f4c3ebfd2e9c7dfb0f0893200",
+    "url": "/LimitingReagentSim/finalassets/images/precipButton2.png"
+  },
+  {
+    "revision": "bfaf0569d8e9e6298f0b8d81cd049e09",
+    "url": "/LimitingReagentSim/finalassets/images/precipGraphAB.png"
+  },
+  {
+    "revision": "3457d3e6b0c9b332322cb26d6c598f2a",
+    "url": "/LimitingReagentSim/finalassets/images/precipGraphCD.png"
+  },
+  {
+    "revision": "62c45d52db468f377e96aedaa3a2bcbb",
+    "url": "/LimitingReagentSim/finalassets/images/precipGraphEF.png"
+  },
+  {
+    "revision": "8c9fc32da3629b3571312cbf06b30dbe",
+    "url": "/LimitingReagentSim/finalassets/images/products.png"
+  },
+  {
+    "revision": "a49ea2a9a6d2056720cfcc0fdd9a2345",
+    "url": "/LimitingReagentSim/finalassets/images/pyHelp.png"
+  },
+  {
+    "revision": "9e0fc06b6809df930d7e5e318ab9563a",
+    "url": "/LimitingReagentSim/finalassets/images/rBox.png"
+  },
+  {
+    "revision": "bb60070a3bf49f63f3dfa5559539472e",
+    "url": "/LimitingReagentSim/finalassets/images/reactantUsed.png"
+  },
+  {
+    "revision": "fd775307448c7efc361849144220389c",
+    "url": "/LimitingReagentSim/finalassets/images/reactants.png"
+  },
+  {
+    "revision": "5e92d910d30aab700b6eea4d8d1b8477",
+    "url": "/LimitingReagentSim/finalassets/images/redArrow.png"
+  },
+  {
+    "revision": "660ac7de1dd3ad5cffa18383474b0129",
+    "url": "/LimitingReagentSim/finalassets/images/scale.png"
+  },
+  {
+    "revision": "a4b27e7805b4d01bb63127b6dc74c4f3",
+    "url": "/LimitingReagentSim/finalassets/images/shirt.png"
+  },
+  {
+    "revision": "be0026d611e1b574800bdf6fd784edd8",
+    "url": "/LimitingReagentSim/finalassets/images/specButton.png"
+  },
+  {
+    "revision": "8096f9041afed3d23257f93c9c257b7e",
+    "url": "/LimitingReagentSim/finalassets/images/specButton2.png"
+  },
+  {
+    "revision": "91558b67cb3156cb52ebe1e18e12f185",
+    "url": "/LimitingReagentSim/finalassets/images/spectrophotometer.png"
+  },
+  {
+    "revision": "d11e87b9a206711db2bac4e4849d15e1",
+    "url": "/LimitingReagentSim/finalassets/images/tempButton.png"
+  },
+  {
+    "revision": "48b4bcf974b3fe38dc830121c511ed98",
+    "url": "/LimitingReagentSim/finalassets/images/tempButton2.png"
+  },
+  {
+    "revision": "a6435e5f9b4bc6566b70320134d8daac",
+    "url": "/LimitingReagentSim/finalassets/images/tempGraphAB.png"
+  },
+  {
+    "revision": "b52c21beb4b00145ff631899ca66736c",
+    "url": "/LimitingReagentSim/finalassets/images/tempGraphCD.png"
+  },
+  {
+    "revision": "f1f257071982ac1ef09672a02fc56167",
+    "url": "/LimitingReagentSim/finalassets/images/tempGraphEF.png"
+  },
+  {
+    "revision": "764f727dccab38417c44e1054c9909b5",
+    "url": "/LimitingReagentSim/finalassets/images/thermoHead.png"
+  },
+  {
+    "revision": "2388819669ea141ec29415955aba7f08",
+    "url": "/LimitingReagentSim/finalassets/images/thermometerScreen.png"
+  },
+  {
+    "revision": "15fed048cea4d4492859b40bf9fddb47",
+    "url": "/LimitingReagentSim/finalassets/images/upArrow.png"
+  },
+  {
+    "revision": "d9de02c3920f87688434ba7ba8ea3f98",
+    "url": "/LimitingReagentSim/finalassets/images/wrongLR.png"
+  },
+  {
+    "revision": "c8196eee501c09e6184ed2e68a69557d",
+    "url": "/LimitingReagentSim/finalassets/text/inputform.html"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/LimitingReagentSim/finalfavicon.ico"
+  },
+  {
+    "revision": "4d859ee7310828506b6d",
+    "url": "/LimitingReagentSim/finalgen.main.68d872b42c1d885d2f57.bundle.js"
+  },
+  {
+    "revision": "6e83cce80712b9bf21e5",
+    "url": "/LimitingReagentSim/finalgen.vendors.a2aedf0e1e15451e0697.bundle.js"
+  },
+  {
+    "revision": "5806aff7c5080b66e0b2cbaef2ee1ac9",
+    "url": "/LimitingReagentSim/finalindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/LimitingReagentSim/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/LimitingReagentSim/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/LimitingReagentSim/finalprecache-manifest.17cbab2b4db0666337d5d3ea8786c398.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
